65a5a69f337079bfe495ffdbce364e7076559277
ahbbollen <a.h.b.bollen@lumc.nl>
2015-02-18 09:07:13 -0500

    Adding owns_group to security model

--- varda/api/resources/groups.py
+++ varda/api/resources/groups.py
@@ -24,7 +24,7 @@ class GroupsResource(ModelResource):
     list_ensure_conditions = [has_role('admin')]
     list_ensure_options = {'satisfy': any}
     
-    get_ensure_conditions = [has_role('admin')]
+    get_ensure_conditions = [has_role('admin'), owns_group]
     get_ensure_options = {'satisfy': any}
     
     add_ensure_conditions = [has_role('admin'), has_role('importer')]
@@ -32,12 +32,12 @@ class GroupsResource(ModelResource):
     add_schema = {'name': {'type': 'string', 'required' = True, 'maxlength': 200}}
     
     # TODO: add owns_group function to security module
-    edit_ensure_conditions = [has_role('admin')]
+    edit_ensure_conditions = [has_role('admin'), owns_group]
     edit_ensure_options = {'satisfy' : any}
     edit_schema = {'name': {'type': 'string', 'required' = True, 'maxlength': 200}}
     
     # TODO: add owns_group function to security module
-    delete_ensure_conditions = [has_role('admin')]
+    delete_ensure_conditions = [has_role('admin'), owns_group]
     delete_ensure_options = {'satisfy': any}
     
     @classmethod
@@ -67,23 +67,44 @@ class GroupsResource(ModelResource):
     @classmethod
     def list_view(cls, *args, **kwargs):
         """
-        Returns a colleciton of samples in the `group_collection` field.        
+        Returns a colleciton of groups in the `group_collection` field.
+        
+        User must have role `admin` to list all groups        
         """
         return super(GroupsResource, cls).list_view(*args, **kwargs)
         
     @classmethod
     def get_view(cls, *args, **kwargs):
+        """
+        Returns a group 
+        User must have role `admin` or own the group
+        """
         return super(GroupsResource, cls).get_view(*args, **kwargs)
     
     @classmethod
     def add_view(cls, *args, **kwargs):
+        """
+        Adds a group to the collection
+        
+        User must have either role `admin` or `importer`
+        """
         kwargs['user'] = g.user
         return super(GroupsResource, cls).add_view(*args, **kwargs)
         
     @classmethod
     def edit_view(cls, *args, **kwargs):
+        """
+        Edits a group
+        
+        User must have role `admin` or be the owner of the group
+        """
         return super(GroupsResource, cls).edit_view(*args, **kwargs)
         
     @classmethod
     def delete_view(cls, *args, **kwargs):
+        """
+        Deletes a group
+        
+        User most have role `admin` or be the owner of the group
+        """
         return super(GroupsResource, cls).delete_view(*args, **kwargs)
