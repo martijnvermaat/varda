4543afbf1b4769a2c5ceb5cb6b97856bd1d64e7b
ahbbollen <a.h.b.bollen@lumc.nl>
2015-02-23 06:59:32 -0500

    Get requests for groups now work

--- varda/api/utils.py
+++ varda/api/utils.py
@@ -16,7 +16,7 @@ from werkzeug.exceptions import HTTPException
 from werkzeug.http import parse_range_header
 
 from ..models import (Annotation, Coverage, DataSource, Sample, Token, User,
-                      Variation)
+                      Variation, Group)
 from .errors import ValidationError
 
 
@@ -119,6 +119,16 @@ def sample_by_uri(app, uri):
     except ValueError:
         return None
     return Sample.query.get(args['sample'])
+    
+def group_by_uri(app, uri):
+	"""
+	Get a group from its URI.
+	"""
+	try:
+		args = parse_args(app, 'api.group_get', uri)
+	except ValueError:
+		return None
+	return Group.query.get(args['group'])
 
 
 def token_by_uri(app, uri):
